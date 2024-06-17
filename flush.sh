d525dffc67946b1c6fadce6d3e4758e10271b166
developercyrus <developer.cyrus@gmail.com>
2024-06-17 19:58:55 +0800

    Update flush.sh

--- flush.sh
+++ flush.sh
@@ -3,6 +3,7 @@
 cat /etc/os-release
 iptables --version
 
+# Ensure you're using the same version of iptables within your container that your host is using. If your host is using iptables-legacy, then your container should also use iptables-legacy.
 IPTABLES_CMD=$(command -v iptables)
 if [ -x "/usr/sbin/iptables-legacy" ]; then
     IPTABLES_CMD="/usr/sbin/iptables-legacy"
